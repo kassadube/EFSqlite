bfa15bd0353fa28ce34b25aa7b0f3963e7e1089e
kassadube <noamsrb@yahoo.com>
2017-09-24 07:22:19 +0300

    test identity

---
+++ EFSqlite/sqlite/AspNet.Identity.SQLite/RoleStore.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AspNet.Identity.SQLite
+{
+    /// <summary>
+    /// Class that implements the key ASP.NET Identity role store iterfaces
+    /// </summary>
+    public class RoleStore<TRole> : IQueryableRoleStore<TRole>
+        where TRole : IdentityRole
+    {
+        private  RoleTable<TRole> roleTable;
+        public SQLiteDatabase Database { get; private set; }
+		private bool DisposeContext;
+		public IQueryable<TRole> Roles
+        {
+            get
+            {
+                return roleTable.GetRoles().AsQueryable<TRole>();
+               // throw new NotImplementedException();
+            }
+        }
+
+
+        /// <summary>
+        /// Default constructor that initializes a new SQLiteDatabase
+        /// instance using the Default Connection string
+        /// </summary>
+        public RoleStore()
+        {
+			DisposeContext = true;
+			new RoleStore<TRole>(new SQLiteDatabase());
+        }
+
+        /// <summary>
+        /// Constructor that takes a SQLiteDatabase as argument 
+        /// </summary>
+        /// <param name="database"></param>
+        public RoleStore(SQLiteDatabase database)
+        {
+			DisposeContext = false;
+			Database = database;
+            roleTable = new RoleTable<TRole>(database);
+        }
+
+        public Task CreateAsync(TRole role)
+        {
+            if (role == null)
+            {
+                throw new ArgumentNullException("role");
+            }
+
+            roleTable.Insert(role);
+
+            return Task.FromResult<object>(null);
+        }
+
+        public Task DeleteAsync(TRole role)
+        {
+            if (role == null)
+            {
+                throw new ArgumentNullException("user");
+            }
+
+            roleTable.Delete(role.Id);
+
+            return Task.FromResult<Object>(null);
+        }
+
+        public Task<TRole> FindByIdAsync(string roleId)
+        {
+            TRole result = roleTable.GetRoleById(roleId) as TRole;
+
+            return Task.FromResult<TRole>(result);
+        }
+
+        public Task<TRole> FindByNameAsync(string roleName)
+        {
+            TRole result = roleTable.GetRoleByName(roleName) as TRole;
+
+            return Task.FromResult<TRole>(result);
+        }
+
+        public Task UpdateAsync(TRole role)
+        {
+            if (role == null)
+            {
+                throw new ArgumentNullException("user");
+            }
+
+            roleTable.Update(role);
+
+            return Task.FromResult<Object>(null);
+        }
+
+        public void Dispose()
+        {
+            if (Database != null)
+            {
+				if (DisposeContext)
+				{
+					Database.Dispose();
+					Database = null;
+				}
+            }
+        }
+
+        public Task<IdentityResult> CreateAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> UpdateAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> DeleteAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRoleIdAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRoleNameAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetRoleNameAsync(TRole role, string roleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetNormalizedRoleNameAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetNormalizedRoleNameAsync(TRole role, string normalizedName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TRole> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TRole> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
